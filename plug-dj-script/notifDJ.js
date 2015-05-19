f9b0fc42e35326a55bf8cf95b352d694c6a700c0
Lunik <guillaume.lunik@gmail.com>
2015-05-19 19:44:15 +0200

    add notifDJ

--- plug-dj-script/notifDJ.js
+++ plug-dj-script/notifDJ.js
@@ -1,3 +1,17 @@
+$chat = $('#chat-messages');
+$chat.append(''+
+	'<div class="cm rsshit message rs-log-green">'+
+		'<div class="badge-box">'+
+			'<i class="icon icon-star-white"></i>'+
+		'</div>'+
+		'<div class="msg">'+
+			'<div class="from">NotifDJ</div>'+
+			'<div class="text">NotifDJ activé</div>'+
+		'</div>'+
+	'</div>'+
+	'');
+$chat.scrollTop($chat.get(0).scrollHeight);
+
 API.on(API.ADVANCE, function(data){
 	console.log(data);
 	var myId=API.getUser().id;
